7b13d239454b69b3c75f347063c0b9d9c588a689
Md.Sabbir Hossain <160324315+sabbir73169@users.noreply.github.com>
2024-02-26 02:49:14 +0600

    Add files via upload

---
+++ first.c
@@ -0,0 +1,15 @@
+void main()
+{
+    int x,y,i;
+    printf("Enter 2 Numbers:");
+    scanf("%d%d",&x,&y);
+    for(i=x<y?x:y;i>0;i--)
+    {
+        if (x%i==0 && y%i==0)
+        {
+            printf("GCD is:%d",i);
+            break;
+        }
+    }
+getch();
+}
